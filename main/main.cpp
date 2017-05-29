cc2c4866cbc8ca423b800077d0e2de4ccc2e2bfc
pedrohmsantana <santana.phm@gmail.com>
2017-05-29 11:11:43 -0300

    Tratamento de erro nao identificado

--- main/main.cpp
+++ main/main.cpp
@@ -281,7 +281,7 @@ int main(int argc, char *argv[])
         S4.push_back(temp_val[6]/5);
         roll.push_back(atan(-xAccel[contador]/zAccel[contador])*180/PI);
         pitch.push_back(3+atan(yAccel[contador]/(sqrt(xAccel[contador]*xAccel[contador]+zAccel[contador]*zAccel[contador])))*180/PI);
-        //cout<<xAccel[contador]<<" "<<yAccel[contador]<<" "<<zAccel[contador]<<" "<<S1[contador]<<" "<<S2[contador]<<" "<<S3[contador]<<" "<<S4[contador]<<" "<<roll[contador]<<" "<<pitch[contador]<<endl;
+        cout<<xAccel[contador]<<" "<<yAccel[contador]<<" "<<zAccel[contador]<<" "<<S1[contador]<<" "<<S2[contador]<<" "<<S3[contador]<<" "<<S4[contador]<<" "<<roll[contador]<<" "<<pitch[contador]<<endl;
         arq3<<xAccel[contador]<<" "<<yAccel[contador]<<" "<<zAccel[contador]<<" "<<S1[contador]<<" "<<S2[contador]<<" "<<S3[contador]<<" "<<S4[contador]<<" "<<roll[contador]<<" "<<pitch[contador]<<endl;
         
         if (abs(roll[contador])>2)

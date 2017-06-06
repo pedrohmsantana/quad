00a1627529fd6dff44aa056e6952e69898498e3c
pedrohmsantana <santana.phm@gmail.com>
2017-06-06 11:22:15 -0300

    Correcao do roll

--- main/main.cpp
+++ main/main.cpp
@@ -284,10 +284,10 @@ int main(int argc, char *argv[])
         //cout<<xAccel[contador]<<" "<<yAccel[contador]<<" "<<zAccel[contador]<<" "<<S1[contador]<<" "<<S2[contador]<<" "<<S3[contador]<<" "<<S4[contador]<<" "<<roll[contador]<<" "<<pitch[contador]<<endl;
         arq3<<xAccel[contador]<<" "<<yAccel[contador]<<" "<<zAccel[contador]<<" "<<S1[contador]<<" "<<S2[contador]<<" "<<S3[contador]<<" "<<S4[contador]<<" "<<roll[contador]<<" "<<pitch[contador]<<endl;
         
-        cout<<roll[contador]<<" "<<lido[0]*0.29*PI/180<<" "<<abs(roll[contador]-lido[0]*0.29*PI/180)<<endl;
-        if (abs(roll[contador]-lido[0]*0.29*PI/180)>2)
+        cout<<roll[contador]<<" "<<lido[0]*0.29<<" "<<abs(roll[contador]-lido[0]*0.29)<<endl;
+        if (abs(roll[contador]-lido[0]*0.29)>2)
         {
-            h=11*tan((roll[contador]*PI/180-lido[0]*0.29*PI/180));
+            h=11*tan((roll[contador]*PI/180-lido[0]*0.29));
             if ((abs(cos(lido[1]*0.29*PI/180)+h/10))>1||
                 (abs(cos(lido[4]*0.29*PI/180)-h/10))>1||
                 (abs(cos(lido[7]*0.29*PI/180)-h/10))>1||

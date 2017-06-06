ae445891c780af5ef98292ad82b23253a855150a
pedrohmsantana <santana.phm@gmail.com>
2017-06-06 11:36:58 -0300

    Roll

--- main/main.cpp
+++ main/main.cpp
@@ -284,10 +284,10 @@ int main(int argc, char *argv[])
         //cout<<xAccel[contador]<<" "<<yAccel[contador]<<" "<<zAccel[contador]<<" "<<S1[contador]<<" "<<S2[contador]<<" "<<S3[contador]<<" "<<S4[contador]<<" "<<roll[contador]<<" "<<pitch[contador]<<endl;
         arq3<<xAccel[contador]<<" "<<yAccel[contador]<<" "<<zAccel[contador]<<" "<<S1[contador]<<" "<<S2[contador]<<" "<<S3[contador]<<" "<<S4[contador]<<" "<<roll[contador]<<" "<<pitch[contador]<<endl;
         
-        cout<<roll[contador]<<" "<<lido[0]*0.29<<" "<<abs(roll[contador]-lido[0]*0.29)<<endl;
-        if (abs(roll[contador]-lido[0]*0.29)>5)
+        //cout<<roll[contador]<<" "<<lido[0]*0.29<<" "<<abs(roll[contador]-lido[0]*0.29)<<endl;
+        if (abs(roll[contador])>5)
         {
-            h=11*tan((roll[contador]*PI/180-lido[0]*0.29));
+            h=11*tan((roll[contador]*PI/180);
             if ((abs(cos(lido[1]*0.29*PI/180)+h/10))>1||
                 (abs(cos(lido[4]*0.29*PI/180)-h/10))>1||
                 (abs(cos(lido[7]*0.29*PI/180)-h/10))>1||

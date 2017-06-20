c8ccfe73ba2fb73aed680728780ae129966c836a
pedrohmsantana <santana.phm@gmail.com>
2017-06-20 17:07:11 -0300

    Mudanca na main para nao realizar o controle de equilibrio

--- main/main.cpp
+++ main/main.cpp
@@ -395,7 +395,7 @@ int main(int argc, char *argv[])
         }
         cout<<"[ "<<cor_fat[0]<<" "<<cor_fat[1]<<" "<<cor_fat[2]<<" "<<cor_fat[3]<<" "<<cor_fat[4]<<" "<<cor_fat[5];
         cout<<" "<<cor_fat[6]<<" "<<cor_fat[7]<<" "<<cor_fat[8]<<" "<<cor_fat[9]<<" "<<cor_fat[10]<<" "<<cor_fat[11]<<" ]\n";
-        //memset(cor_fat, 0, sizeof cor_fat);
+        memset(cor_fat, 0, sizeof cor_fat);
         contador++;
     }
     arq.close();
